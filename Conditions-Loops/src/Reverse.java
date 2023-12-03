2db4dc9e88366a13b530a690fc1e01451e12c9f6
Umoh Samuel Godwin <umohsg@gmail.com>
2023-12-03 23:12:49 +0100

    Conditionals and loops
    Signed-off-by: Umoh Samuel Godwin <umohsg@gmail.com>

---
+++ Conditions-Loops/src/Reverse.java
@@ -0,0 +1,14 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int num = 28479;
+
+        int answer = 0;
+        while (num > 0){
+            int rem = num % 10;
+            num /= 10;
+
+            answer = answer * 10 + rem;
+        }
+            System.out.println(answer);
+    }
+}
